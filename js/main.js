89ed4cf5b6cabf754c6b09a9cda170ad23941ef4
EC2 Default User <ec2-user@ip-172-31-18-179.us-east-2.compute.internal>
2021-05-26 01:50:52 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+	    autoplay: true,         
+	    autoplaySpeed: 4000,　  
+	    speed: 800,		　 
+	    dots: true,　　　　　　
+	    arrows: true,
+	    infinite: true,　　　
+	    pauseOnHover: false,  
+	});
